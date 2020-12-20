4cbcf86066ae8d543569c4951cd2c50eda2647e5
AKASH SANTOSH CHAUDHARI <65148480+akash2941@users.noreply.github.com>
2020-12-21 01:46:37 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAssignment.Models
+{
+    public class User
+    {
+
+        [Key]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please fill the feild")]
+        [MaxLength(10), MinLength(4)]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please fill the feild")]
+        [MaxLength(10), MinLength(4)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please fill the feild")]
+        [MaxLength(10), MinLength(4)]
+        public string FullName  { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please fill the feild")]
+        public string Email { get; set; }
+
+        public int CityId { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Please fill the feild")]
+        [MaxLength(10), MinLength(10)]
+        public string PhoneNo { get; set; }
+        public IEnumerable<SelectListItem> Cities { get; set; }
+        public bool IsActive { set; get; }
+    }
+}
